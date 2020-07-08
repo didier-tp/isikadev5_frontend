5b5e4e98dad3f3215bfd9873323fb8b806df69a2
didier <didier@d-defrance.fr>
2020-07-08 14:29:52 +0200

    ws rest en mode get

--- tpNodeJs/myNodeServer/lancer_node.bat
+++ tpNodeJs/myNodeServer/lancer_node.bat
@@ -1,7 +1,8 @@
 REM node helloWorld.js
 REM node basic_http_server.js
 REM nodemon basic_http_server.js
-node lancer_calcul.js
+REM node lancer_calcul.js
 REM node first_express_server.js
 REM nodemon server.js
+node server.js
 pause
