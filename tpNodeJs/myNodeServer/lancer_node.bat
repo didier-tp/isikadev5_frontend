3710d072e01ae48885f48f987fc5c9bb953f2869
didier <didier@d-defrance.fr>
2020-07-08 11:45:25 +0200

    exports/require sur calcul

--- tpNodeJs/myNodeServer/lancer_node.bat
+++ tpNodeJs/myNodeServer/lancer_node.bat
@@ -1,7 +1,7 @@
 REM node helloWorld.js
 REM node basic_http_server.js
-nodemon basic_http_server.js
-REM node lancer_calcul.js
+REM nodemon basic_http_server.js
+node lancer_calcul.js
 REM node first_express_server.js
 REM nodemon server.js
 pause
