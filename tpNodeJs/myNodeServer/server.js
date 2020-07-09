238f60ddc8d7531816ba2141d7b4b3d005dc9b04
didier <didier@d-defrance.fr>
2020-07-09 12:16:26 +0200

    avec autorisations cors

--- tpNodeJs/myNodeServer/server.js
+++ tpNodeJs/myNodeServer/server.js
@@ -4,15 +4,17 @@ var produitApiRoutes = require('./produit-api-routes');
 var bodyParser = require('body-parser');
 var app = express();
 
-/*
+
 app.use(function(req, res, next) {
 res.header("Access-Control-Allow-Origin", "*");
 //ou avec "www.xyz.com" à la place de "*" en production
+res.header("Access-Control-Allow-Methods",
+           "POST, GET, PUT, DELETE, OPTIONS"); //default: GET, ...
 res.header("Access-Control-Allow-Headers",
 "Origin, X-Requested-With, Content-Type, Accept");
 next();
 });
-*/
+
 
 //support parsing of JSON post data
 var jsonParser = bodyParser.json() ;
