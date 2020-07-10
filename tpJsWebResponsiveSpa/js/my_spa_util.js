9710a106ad9339b388f982e92f14cdb4efdde0fd
didier <didier@d-defrance.fr>
2020-07-10 17:20:39 +0200

    avec chargement plus fiable des sous pages

--- tpJsWebResponsiveSpa/js/my_spa_util.js
+++ tpJsWebResponsiveSpa/js/my_spa_util.js
@@ -3,6 +3,9 @@ function loadJsScript( src ,callback /*facultative*/ ) {
   var es = document.querySelector('script[src="'+src+'"]');
   if(es!=null){
 	  //console.log("already loaded script with src="+src);
+	  if(callback) {
+		   callback(); //ex: chargement page + eval de startXy()
+	  }
   }else{
 	  var s = document.createElement('script');
 	  s.setAttribute( 'src', src );
@@ -28,14 +31,16 @@ function loadSubPart(idSubPartOfMainPage,urlSubPage,postLoadingActionToEval){
 function initMainPageAfterLoad(){
 	var btnP1 = document.getElementById("idP1");
 	btnP1.addEventListener('click', function(event) {
-	    loadJsScript("js/euroToFranc.js");
-		loadSubPart("idAlternativeSubPart","sp_euroToFranc.html","startEuroToFranc()")
+	    loadJsScript("js/euroToFranc.js", function () {
+		   loadSubPart("idAlternativeSubPart","sp_euroToFranc.html","startEuroToFranc()")
+		});
 	});
 	var btnP2 = document.getElementById("idP2");
 	btnP2.addEventListener('click', function(event) {
 	    loadJsScript("lib/chart.2.7.3.min.js");
-		loadJsScript("js/graph.js");
-		loadSubPart("idAlternativeSubPart","sp_graph.html","startGraph()")
+		loadJsScript("js/graph.js", function () {
+			loadSubPart("idAlternativeSubPart","sp_graph.html","startGraph()")
+		});
 	});
 }
 window.addEventListener('load', initMainPageAfterLoad);
