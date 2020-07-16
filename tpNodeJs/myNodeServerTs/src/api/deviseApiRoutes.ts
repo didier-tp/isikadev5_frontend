d2000f963e0290913ac74f626104831d294b81ad
didier <didier@d-defrance.fr>
2020-07-16 16:12:19 +0200

    avec test et async/await

--- tpNodeJs/myNodeServerTs/src/api/deviseApiRoutes.ts
+++ tpNodeJs/myNodeServerTs/src/api/deviseApiRoutes.ts
@@ -19,85 +19,71 @@ function initDeviseService() : DeviseDataService {
 
 // .../devise-api/public/devise/EUR ou ...
 deviseApiRouter.route('/devise-api/public/devise/:code')
-.get(function(req :Request, res :Response , next: NextFunction){
+.get(asyncToResp(async function(req :Request, res :Response , next: NextFunction){
     let codeDevise = req.params.code;
-    //appeler deviseService.findById(codeDevise)
-    //renvoyer  devise via res.send() dans .then();
-    deviseService.findById(codeDevise)
-    .then((devise)=>{ res.send(devise);} )
-    .catch((err)=>{ res.status(404).send(err);})
-});
+    let devise = await deviseService.findById(codeDevise)
+    return devise;
+}));
 
 // http://localhost:8282/devise-api/public/devise renvoyant tout [ {} , {}]
 // http://localhost:8282/devise-api/public/devise?changeMini=1.1 renvoyant [{}] selon critere
 deviseApiRouter.route('/devise-api/public/devise')
-.get(function(req :Request, res :Response , next: NextFunction ) {
+.get(asyncToResp(async function(req :Request, res :Response , next: NextFunction ) {
     let  changeMini :number = Number(req.query.changeMini);
-    //appeler  deviseService.findAll();
-    //renvoyer  deviseArray via res.send() dans .then();
-    deviseService.findAll()
-    .then((deviseArray)=>{ res.send(deviseArray);} )
-    .catch((err)=>{ res.status(404).send( err );})
-});
+    let deviseArray = await deviseService.findAll();
+    if(changeMini){
+            //filtrage selon critère changeMini:
+            deviseArray = deviseArray.filter((dev)=>dev.change >= changeMini);
+        }
+    return deviseArray;
+}));
 
 // .../devise-api/public/convert?source=EUR&target=USD&amount=100 renvoyant { ... } 
 deviseApiRouter.route('/devise-api/public/convert')
-.get(async function(req :Request, res :Response , next: NextFunction){
+.get(asyncToResp(async function(req :Request, res :Response , next: NextFunction){
     const  codeSrc :string = <any> req.query.source;
     const  codeTarget:string = <any> req.query.target;
     const  amount = Number(req.query.amount);
-    //appeler deviseService.findById(codeSrc);
-    //appeler  deviseService.findById(codeTarget) dans premier .then()
-    //renvoyer la chose ci-après via res.send() dans second  .then(); 
-	let deviseSrc : Devise=null;
-    deviseService.findById(codeSrc)
-    .then( (deviseSource)=>{ deviseSrc = deviseSource;
-                             return deviseService.findById(codeTarget);} )
-    .then((deviseTarget) => { res.send(
-                                        { source : codeSrc,
-                                          target : codeTarget,
-                                          amount : amount,
-                                        result : amount * deviseTarget.change / deviseSrc.change
-                                        });
-     })
-     .catch((err)=>{ res.status(500).send(err );})
-});
+	//*********** A FAIRE EN TP : **********
+    //appeler via await  deviseService.findById(codeSrc); et recuperer
+	//le resultat dans deviseSrc 
+	//appeler via await  deviseService.findById(codeTarget); et recuperer
+	//le resultat dans deviseTarget
+	//**************************************
+    const deviseSrc = await deviseService.findById(codeSrc);
+    const deviseTarget = await deviseService.findById(codeTarget)
+    return { source : codeSrc,
+             target : codeTarget,
+             amount : amount,
+             result : amount * deviseTarget.change / deviseSrc.change
+    };
+}));
 
 
 //POST ... with body { "code": "M1" , "nom" : "monnaie1" , "change" : 1.123 }
 deviseApiRouter.route('/devise-api/private/role-admin/devise')
-.post(function(req :Request, res :Response , next: NextFunction ) {
+.post(asyncToResp(async function(req :Request, res :Response , next: NextFunction ) {
     let  devise :Devise =  req.body ; //as javascript object via jsonParser
-    //appeler deviseService.insert(devise);
-    //renvoyer  savedDevise via res.send() dans .then();
-    deviseService.insert(devise)
-    .then((savedDevise)=>{ res.send(savedDevise);} )
-    .catch((err)=>{ res.status(500).send(  err );})
-});
+    let savedDevise = await deviseService.insert(devise);
+                      // await deviseService.saveOrUpdate(devise);
+    return savedDevise;
+}));
 
 //PUT ... with body { "code": "USD" , "nom" : "dollar" , "change" : 1.1 }
 deviseApiRouter.route('/devise-api/private/role-admin/devise')
-.put(function(req :Request, res :Response , next: NextFunction ) {
+.put(asyncToResp(async  function(req :Request, res :Response , next: NextFunction ) {
     let  devise :Devise =  req.body ; //as javascript object
-	//appeler deviseService.update(devise);
-    //renvoyer  updatedDevise via res.send() dans .then();
-    deviseService.update(devise)
-    .then((updatedDevise)=>{ res.send(updatedDevise);} )
-    .catch((err)=>{ res.status(500).send( err );})
-});
+    let updatedDevise = await deviseService.update(devise);
+    return updatedDevise;
+}));
 
 // DELETE http://localhost:8282/devise-api/private/role_admin/devise/EUR
 deviseApiRouter.route('/devise-api/private/role-admin/devise/:code')
-.delete(function(req :Request, res :Response , next: NextFunction ) {
+.delete(asyncToResp(async  function(req :Request, res :Response , next: NextFunction ) {
     let codeDevise = req.params.code;
-    //appeler  deviseService.deleteById(codeDevise)
-    //renvoyer la chose ci-après via res.send() dans .then();
-    deviseService.deleteById(codeDevise)
-	.then ( () => {
-        res.send({ "action" : "devise with code="+codeDevise + " was deleted"});
-       })
-    .catch((err)=>{ res.status(500).send(err);})
-});
+    await deviseService.deleteById(codeDevise)
+    return{ "action" : "devise with code="+codeDevise + " was deleted"};
+}));
 
 
 
