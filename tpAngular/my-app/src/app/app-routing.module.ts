6a34bbbbcfb4aa86b5d6a5d9ba374182e562bed2
didier <didier@d-defrance.fr>
2020-07-20 15:58:46 +0200

    avec routing

--- tpAngular/my-app/src/app/app-routing.module.ts
+++ tpAngular/my-app/src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { LoginComponent } from './login/login.component';
+import { BasicComponent } from './basic/basic.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'ngr-welcome', component: WelcomeComponent },
+  { path: '', redirectTo: '/welcome', pathMatch: 'full'},
+  { path: 'ngr-login', component: LoginComponent },
+  { path: 'ngr-basic', component: BasicComponent }
+];
+
+//NB: les path peuvent commencer par "ngr-" .
+//ceci permet un éventuel filtrage d'url à un niveau
+//intermédiaire (ex: nginx)
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
