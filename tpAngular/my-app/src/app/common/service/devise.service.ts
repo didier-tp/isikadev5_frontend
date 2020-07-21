94e666c3c7c7cd05ddc79a9dc54343e91b2e6816
didier <didier@d-defrance.fr>
2020-07-21 15:18:31 +0200

    appel ws get

--- tpAngular/my-app/src/app/common/service/devise.service.ts
+++ tpAngular/my-app/src/app/common/service/devise.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Devise } from '../data/devise';
 import { Observable, of } from 'rxjs';
 import {  map} from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 
 //{"source":"EUR","target":"USD","amount":200,"result":219.78021978021977}
 interface ConvResult{
@@ -23,14 +24,20 @@ export class DeviseService {
   ];
 
   public getAllDevises() : Observable<Devise[]>{
-    return of(this.devises); //version préliminaire (maintenant asynchrone)
-  }
+    //return of(this.devises); //version préliminaire (maintenant asynchrone)
+      let baseUrl = "http://localhost:8282/devise-api/public/devise";
+      let wsUrl = `${baseUrl}`;
+      return this.http.get<Devise[]>(wsUrl);
+    }
 
   public convertir(codeDeviseSrc : string, 
                   codeDeviseTarget : string ,
                    montant: number) : Observable<number> {
-                    // return of(0.89675); //version temporaire (maintenant asynchrone)
-  return this.http.get....
+                    //return of(0.89675); //version temporaire (maintenant asynchrone)
+      let baseUrl = "http://localhost:8282/devise-api/public/convert";
+      let wsUrl = `${baseUrl}?source=${codeDeviseSrc}&target=${codeDeviseTarget}&amount=${montant}`;
+      console.log("wsUrl="+wsUrl);
+      return this.http.get<ConvResult>(wsUrl)
                   .pipe(
                      map( (resConv:ConvResult)=>resConv.result)
                   );
@@ -38,5 +45,5 @@ export class DeviseService {
   }
 
   //injecter ici http de type HttpClient
-  constructor() { }
+  constructor(private http: HttpClient){ }
 }
